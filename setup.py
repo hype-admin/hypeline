fb11e770aba2d3802a6d748a09bf12b5538a78d2
Hype admin <adm@hype.cg>
2023-01-17 03:47:47 -0300

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hypeline',
     version='0.1',
-    py_modules=find_packages(),
+    py_modules=find_packages(where='.'),
     install_requires=[
         'gspread',
         'dropbox',
