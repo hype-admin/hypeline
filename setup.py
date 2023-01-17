b782478f201707eaaff3c7db7ce8cb9d443fc405
Hype admin <adm@hype.cg>
2023-01-17 02:56:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='hypeline',
     version='0.1',
     py_modules=[
-        'hypeline.hypeline',
-        'hypeline.hypeline.files_tools',
-        'hypeline.hypeline.maya_ascii',
-        'hypeline.hypeline.maya_prompt'
+        'hypeline',
+        'hypeline.files_tools',
+        'hypeline.maya_ascii',
+        'hypeline.maya_prompt'
         ],
     install_requires=[
         'gspread',
