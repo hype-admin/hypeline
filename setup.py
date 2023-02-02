ceb1bc40fce39a44c5fdec3a3d39aab995530d01
Hype admin <adm@hype.cg>
2023-02-01 22:56:20 -0300

    add builder to packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hypeline',
     version='0.1',
-    packages=['hypeline'],
+    packages=['hypeline', 'builder'],
     install_requires=[
         'gspread',
         'dropbox',
